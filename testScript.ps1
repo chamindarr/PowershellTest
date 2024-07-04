2815804639144bf0b1123ad4527511ec785f6f4a
chamindarr <53171439+chamindarr@users.noreply.github.com>
2024-07-04 10:22:48 +0400

    Update testScript.ps1
    change the order

--- testScript.ps1
+++ testScript.ps1
@@ -1,5 +1,5 @@
 Get-NetIPAddress | Format-Table
 Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User
+Write-Host "This is testing Powershell logs, End of the Script"
 Start-Process 'calc.exe'
 PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('You Are Hacked')"
-Write-Host "This is testing Powershell logs, End of the Script"
