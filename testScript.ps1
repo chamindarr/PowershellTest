bbe7f131c4a22fd480141d49089981679af74061
chamindarr <53171439+chamindarr@users.noreply.github.com>
2024-07-04 09:30:38 +0400

    Update testScript.ps1
    updated the script to have only console message

--- testScript.ps1
+++ testScript.ps1
@@ -1,4 +1,5 @@
-Get-NetIPAddress >> c:\tmp\IPAddress
-Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User | Format-List | Out-File -Force -FilePath "c:\tmp\autorun.txt"
+Get-NetIPAddress | Format-Table
+Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User
 Start-Process 'calc.exe'
 PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('You Are Hacked')"
+Write-Host "This is testing Powershell logs, End of the Script"
