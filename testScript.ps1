1d71d452573c14fe116519a3e240146cf5c69654
chamindarr <53171439+chamindarr@users.noreply.github.com>
2024-08-01 08:49:19 +0400

    Update testScript.ps1
    add scan drive

--- testScript.ps1
+++ testScript.ps1
@@ -1,5 +1,6 @@
 Get-NetIPAddress | Format-Table
 Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User
+Get-PSDrive -PSProvider FileSystem | Select-Object name, @{n="Root"; e={if ($_.DisplayRoot -eq $null) {$_.Root} else {$_.DisplayRoot}}}
 Write-Host "This is testing Powershell logs, End of the Script"
 Start-Process 'calc.exe'
 PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('You Are Hacked')"
